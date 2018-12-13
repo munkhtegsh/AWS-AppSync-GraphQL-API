479458c5426f14eb82846d2f676e4696c831964b
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-12-13 00:26:34 -0800

    Execute Mutations to an AWS AppSync GraphQL API from React app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 
 import gql from 'graphql-tag';
 import { graphql, compose } from 'react-apollo';
+import { graphqlMutation } from 'aws-appsync-react';
+
+const CreateTodo = gql`
+  mutation($title: String!, $completed: Boolean) {
+    createTodo(input: {
+      title: $title
+      completed: $completed
+    }) {
+      id title completed
+    }
+  }
+`
 
 const ListTodos = gql`
   query {
@@ -16,6 +28,16 @@ const ListTodos = gql`
 `
 
 class App extends Component {
+  state = { todo: '' }
+
+  addTodo = () => {
+    if (this.state.todo === '') return
+    const todo = {
+      title: this.state.todo, completed: false
+    }
+    this.props.createTodo(todo)
+    this.setState({ todo: '' })
+  }
   render() {
     return (
       <div className="App">
@@ -33,6 +55,12 @@ class App extends Component {
             Learn React
           </a>
         </header>
+      <input
+        onChange={e => this.setState({ todo: e.target.value })}
+        value={this.state.todo}
+        placeholder='Todo name'
+      />
+      <button onClick={this.addTodo}> Add todo </button>
       {
         this.props.todos.map((item, i) =>
           <p key={i}> { item.title } </p>
@@ -44,6 +72,7 @@ class App extends Component {
 }
 
 export default compose(
+  graphqlMutation(CreateTodo, ListTodos, 'Todo'),
   graphql(ListTodos, {
     options: {
       fetchPolicy: 'cache-and-network'
